e100380cefd2fcb2095cd14b3e0efab5e97a551c
AofPatiphan <patiphan.ma@hotmail.com>
2022-03-23 00:44:40 +0700

    setup project

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+// // Create Table
+// const { sequelize } = require('./dbs/models/index');
+// sequelize.sync({ force: false });
+
+require('dotenv').config();
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(
+    express.urlencoded({
+        limit: '50mb',
+        extended: true,
+    })
+);
+
+// Error handling
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'resource not found on this server' });
+});
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    let code = 500;
+    if (err.name === 'JsonWebTokenError') {
+        code = 401;
+    }
+    if (err.name === 'TokenExpiredError') {
+        code = 401;
+    }
+    if (process.env.NODE_ENV === 'development') {
+        res.status(code).json({ message: err.message });
+    } else {
+        res.status(code.json({ message: 'something wrong' }));
+    }
+});
+
+const jwt = require('jsonwebtoken');
+const http = require('http');
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 8000, () =>
+    console.log(`server run on port ${process.env.PORT}`)
+);
